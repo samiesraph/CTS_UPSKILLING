9f2cc7aa6a4f6d17f2e94812a53198e6eff2688d
samiesraph <samiesraphael5@gmail.com>
2026-06-02 12:13:23 +0530

    Delete JAVA directory

--- JAVA/OperatorPrecedence.java
+++
@@ -1,9 +0,0 @@
-public class OperatorPrecedence {
-    public static void main(String[] args) {
-        int result = 10 + 5 * 2;
-        System.out.println("Expression: 10 + 5 * 2");
-        System.out.println("Result: " + result);
-        System.out.println("Order of operations: multiplication (*) happens before addition (+).");
-    }
-}
-
